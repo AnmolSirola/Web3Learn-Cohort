2c85eb33511225147e42f1cec4cd53201a0a7ee4
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2022-08-27 21:05:44 +0530

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -10,15 +10,13 @@ contract Voting {
     mapping (address => uint) public voteCount;
 
     function accept() public payable {
-        
-        // Error out if anything other than 1 ether is sent
+      
         require(msg.value == 1 ether, string.concat("please send 1 ether, not ", Strings.toString(msg.value)));
 
     }
 
     function vote() public payable {
 
-         //each account votes only once
         require(voteCount[msg.sender] < 1, string.concat("You are  allowed to vote only once!"));
 
         accept();
